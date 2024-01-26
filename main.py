005094cd7bccd1f049d50fc4df816c0250d1a191
shwetabangotra <135050236+shwetabangotra@users.noreply.github.com>
2024-01-26 15:08:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import qrcode
+upi_id = input("Enter your UPI ID = ")
+
+# upi://pay?pa=UPI_ID&pn=NAME&am=Amount&cu=CURRENCY&tn=MESSAGE
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
